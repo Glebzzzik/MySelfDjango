beaf58f75fc3761f1f48451e49278a4f825d55af
gleb <glebgi23@mail.ru>
2022-09-24 19:06:24 +0300

    requirements.txt added

--- coolsite/myself/views.py
+++ coolsite/myself/views.py
@@ -1,9 +1,3 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import View
-from django.views.generic.base import TemplateView
 
 
-# def index(request):
-    # return HttpResponse('Страница приложения myself')
-    # return render(request, 'myself/index.html')
